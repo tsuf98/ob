7fe563ef654c2e02e13acc54f52abda35293f027
tsuf98 <tsuf@equitybee.com>
2021-09-06 18:35:05 +0300

    initiating things

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const app = require('express')();
 const mongoose = require('mongoose');
 
 const PORT = 8080 || process.env.PORT;
+const mongoUri =
+  'mongodb+srv://ob-app:orbaeven13@cluster0.auoth.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'; //TODO: remove this password from here and put it in a safe place
 
+mongoose.connect(mongoUri, () =>
+  console.log('DB connection established')
+);
 
-app.get('/', (req,res) => {
-    res.send('working!')
-})
+app.get('/', (req, res) => {
+  res.send('working!');
+});
 
-app.listen(PORT, () => console.log(`Amazing! app is listening on port ${PORT} ^_^ `))
+app.listen(PORT, () =>
+  console.log(`Amazing! app is listening on port ${PORT} ^_^ `)
+);
